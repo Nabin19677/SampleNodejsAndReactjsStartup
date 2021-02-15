910734084debdbb0ede6b689a08760b0a2f33691
Anil Khadka <anil.khadka3777@gmail.com>
2021-02-15 18:23:33 +0545

    cleaned

--- backend/index.js
+++ backend/index.js
@@ -5,5 +5,5 @@ const app = express();
 app.use(express.static('public'))
 
 app.listen(3000, () => {
-    console.log("application started listening");
+    console.log("application started listening at 3000");
 })
